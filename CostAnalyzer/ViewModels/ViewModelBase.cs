28057ea5a65708314ab07bd15bb70cf78440c205
Monika <monarus13@gmail.com>
2024-05-24 20:10:22 +0200

    Initial commit - AvaloniaUI project creates

---
+++ CostAnalyzer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace CostAnalyzer.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ CostAnalyzer/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace CostAnalyzer.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
