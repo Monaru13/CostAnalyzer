6d71622f88cfb681dd97ccab16736903cf405f3e
Monika <monarus13@gmail.com>
2024-05-25 22:47:01 +0200

    Refactor of CostListViewModel

--- src/ViewModels/CostListViewModel.cs
+++ src/ViewModels/CostListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using CostAnalyzer.Models;
 using CostAnalyzer.Services;
 
@@ -13,6 +14,6 @@ namespace CostAnalyzer.ViewModels
             _repository = repository;
         }
 
-        public ObservableCollection<CostItem> Items => _repository.GetItems();
+        public IEnumerable<CostItem> Items => _repository.GetItems();
     }
 }
